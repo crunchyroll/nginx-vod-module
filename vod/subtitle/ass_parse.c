c14ea24c5525f93e2fbafe2659ccb70a0e84a918
rmikhael <rmikhael@ellation.com>
2018-09-27 12:59:29 -0700

    correct initialization of Default style

--- vod/subtitle/ass_parse.c
+++ vod/subtitle/ass_parse.c
@@ -662,23 +662,28 @@ void ass_free_track(vod_pool_t* pool, ass_track_t *track)
  */
 static void set_default_style(ass_style_t *style)
 {
-    style->Name             = strdup("Default");
-    style->FontName         = strdup("Arial");
-    style->FontSize         = 18;
-    style->PrimaryColour    = 0xffffff00;
-    style->SecondaryColour  = 0x00ffff00;
-    style->OutlineColour    = 0x00000000;
-    style->BackColour       = 0x00000080;
-    style->Bold             = 200;
-    style->ScaleX           = 1.0;
-    style->ScaleY           = 1.0;
-    style->Spacing          = 0;
-    style->BorderStyle      = 1;
-    style->Outline          = 2;
-    style->Shadow           = 3;
-    style->Alignment        = 2;
-    style->MarginL = style->MarginR = style->MarginV = 20;
+    style->Name                 = strdup("Default");
+    style->FontName             = strdup("Arial");
+    style->FontSize             = 18;
+    style->PrimaryColour        = 0xffffff00;
+    style->SecondaryColour      = 0x00ffff00;
+    style->OutlineColour        = 0x00000000;
+    style->BackColour           = 0x00000080;
+    style->Bold                 = FALSE;
+    style->Italic               = FALSE;
+    style->Underline            = FALSE;
+    style->StrikeOut            = FALSE;
     style->bRightToLeftLanguage = FALSE;
+    style->ScaleX               = 100.0;
+    style->ScaleY               = 100.0;
+    style->Spacing              = 0.0;
+    style->Angle                = 0.0;
+    style->BorderStyle          = 1;
+    style->Outline              = 2;
+    style->Shadow               = 0;
+    style->Alignment            = 2;
+    style->MarginL = style->MarginR = style->MarginV = 20;
+    style->Encoding             = 0;
 }
 
 static long long string2timecode(char *p)
